c3a8511a3902fbac11500652def32a4c7bd302e9
webcodejunkie <webcodejunkie@gmail.com>
2022-05-23 13:58:39 -0400

    added origin policy

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 });
 
 
+// Cors Policy
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:3000', 'https://webcodejunkie.github.io'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application does not allow acess from origin' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 
 
 // Middlewares
