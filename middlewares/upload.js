2894c35dbe1429a198c391fbad3d0a426f490303
webcodejunkie <webcodejunkie@gmail.com>
2022-06-23 13:00:56 -0400

    edited image upload function

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,8 +1,9 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads')
+    cb(null, path.join(__dirname, '/uploads/'));
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now())
