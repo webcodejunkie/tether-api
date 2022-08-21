b36b17ee6d24ca24ac85d6c4f07bf594712335c7
webcodejunkie <webcodejunkie@gmail.com>
2022-08-21 11:48:17 -0400

    fix tpo in upload method

--- routes/user.js
+++ routes/user.js
@@ -18,10 +18,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, path.join(__dirname, 'uploads/'));
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + file.originalname);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   }
 });
 
