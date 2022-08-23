b4b013d43cda6bcc2f93434c5d745474dbbc08a9
webcodejunkie <webcodejunkie@gmail.com>
2022-08-22 23:30:08 -0400

    move uploads folder

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'uploads/'))
+    cb(null, path.join(__dirname + 'uploads/'))
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now())
