d23e9d3d18fe615ffb08567faada894625ea919d
webcodejunkie <webcodejunkie@gmail.com>
2022-08-28 20:47:54 -0400

    edit typo

--- libs/upload.js
+++ libs/upload.js
@@ -12,7 +12,16 @@ const s3 = new S3Client({
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true)
+  } else {
+    cb(new Error("Invalid file type, only JPEG and PNG is allowed"), false);
+  }
+};
+
 const upload = multer({
+  fileFilter,
   storage: multerS3({
     acl: "public-read",
     s3: s3,
@@ -22,7 +31,7 @@ const upload = multer({
       cb(null, { fieldName: file.originalname + '-' + file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString() + file.originalname);
+      cb(null, file.originalname);
     }
   })
 });
