e272c2b27ede0e5fde65c90f1e2a4beeb426fa7f
webcodejunkie <webcodejunkie@gmail.com>
2022-09-08 10:27:10 -0400

    io.listen function added

--- index.js
+++ index.js
@@ -65,6 +65,8 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
+io.listen(3000);
+
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log("Listening on port " + port);
