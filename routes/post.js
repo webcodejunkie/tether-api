57b721567185a61a103ece1549347e623efc7ff2
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2022-10-10 11:33:27 -0400

    new request for user to fetch individual posts

--- routes/post.js
+++ routes/post.js
@@ -8,33 +8,45 @@ const Posts = Models.Post;
 
 // Post A Message
 router.post('/post/:UserID/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Posts
-    .create({
-      'from.UserID': req.body.UserID,
-      'from.Avatar': req.body.Avatar,
-      'from.Username': req.body.Username,
-      content: req.body.content,
-    })
-    .then((post) => {
-      console.log(post);
-      res.status(201).json(post);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+	Posts
+		.create({
+			'from.UserID': req.body.UserID,
+			'from.Avatar': req.body.Avatar,
+			'from.Username': req.body.Username,
+			content: req.body.content,
+		})
+		.then((post) => {
+			console.log(post);
+			res.status(201).json(post);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
 
 // Get Feed Of All Posts
 router.get('/feed', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Posts.find({}).sort({ createdAt: -1 })
-    .then((posts) => {
-      res.status(201).json(posts);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+	Posts.find({}).sort({ createdAt: -1 })
+		.then((posts) => {
+			res.status(201).json(posts);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
 });
+
+// Get Users Posts
+router.get('/:UserID/posts', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Posts.find({ 'from.UserID ': req.body.UserID }, { createdAt: -1 })
+		.then((posts) => {
+			res.status(201).json(posts);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error: ' + err);
+		});
+})
 
 module.exports = router;
