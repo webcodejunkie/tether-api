017df8b136176f80f11ec9bd6e39721ee52f8b45
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2022-10-12 13:23:13 -0400

    correct socket.io implementation for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"),
 	app = express(),
 	server = require("http").createServer(app),
-	io = require("socket.io").listen(server);
+	io = require("socket.io")(server);
 
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
