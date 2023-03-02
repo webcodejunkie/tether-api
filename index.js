c53689ed400945b8338ba024ff6c87034d60b073
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2023-03-02 11:30:54 -0500

    update cors policy

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+// Cors Policy
+app.use(cors({
+	origin: (origin, callback) => {
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) === -1) {
+			let message = 'The CORS policy for this application does not allow access from origin' + origin;
+			return callback(new Error(message), false);
+		}
+		return callback(null, true);
+	}
+}));
+
 require("dotenv").config();
 // Imported Routes
 const userRoute = require('./routes/user');
